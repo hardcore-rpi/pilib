e29c01b27379c9f4fd25665f0c8cfbfa77423939
concefly <concefly@foxmail.com>
2020-10-31 17:10:18 +0800

    fix: 一阶滤波改善 capturer 抖动

--- packages/capturer/app/service/Capturer.ts
+++ packages/capturer/app/service/Capturer.ts
@@ -10,6 +10,12 @@ declare module 'ah-server' {
 /** 人脸捕捉服务 */
 export class Capturer extends Service {
   private lastFaceCnt = 0;
+  private lastValue = 0;
+
+  // 一阶滤波系数
+  // 系数越小，滤波结果越平稳，但是灵敏度越低；滤波系数越大，灵敏度越高，但是滤波结果越不稳定
+  private readonly fa = 0.2;
+  private readonly threshold = 0.9;
 
   public snapshot?: Snapshot;
 
@@ -19,17 +25,30 @@ export class Capturer extends Service {
       this.snapshot = snapshot;
 
       const { detection } = await snapshot.detectAllFaces();
-      const faceCount = detection.objects.length;
 
-      if (this.lastFaceCnt < faceCount) {
-        // 有人进入画面，上传
-        const ds = await snapshot.copy({ markAllFaces: true });
-        await this.service.uploader.upload(ds);
-      }
+      // 一阶滤波
+      const currentValue = this.fa * detection.objects.length + (1 - this.fa) * this.lastValue;
+      this.lastValue = currentValue;
 
-      if (faceCount !== this.lastFaceCnt) {
-        this.logger.info(`faceCount ${this.lastFaceCnt} ${faceCount - this.lastFaceCnt}`);
-        this.lastFaceCnt = faceCount;
+      const delta = currentValue - this.lastFaceCnt;
+
+      // 差值大于阈值
+      if (Math.abs(delta) > this.threshold) {
+        // 记录阶段值
+        this.lastFaceCnt = Math.round(currentValue);
+
+        const faceIn = delta > 0;
+        this.logger.info(
+          `faceCount ${this.lastFaceCnt} ${
+            faceIn ? `+${delta.toFixed(2)}` : `-${delta.toFixed(2)}`
+          }`
+        );
+
+        if (faceIn) {
+          // 有人进入画面，上传
+          const ds = await snapshot.copy({ markAllFaces: true });
+          await this.service.uploader.upload(ds);
+        }
       }
     } catch (e) {
       this.logger.error(e.message || e);
