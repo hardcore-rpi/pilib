78d48685cc71e1a6500cde4a11a0209f4d593828
concefly <concefly@foxmail.com>
2020-12-12 18:49:14 +0800

    feat: capturer install 支持 linux arm32

--- packages/capturer/install/opencv.js
+++ packages/capturer/install/opencv.js
@@ -33,21 +33,34 @@ function exec(command, args, opt = {}) {
  */
 const platform = os.platform();
 
-console.log('current platform:', platform);
+/**
+ * @see https://nodejs.org/api/os.html#os_os_platform
+ * @type {'arm' | 'arm64' | 'x64'};
+ */
+const arch = os.arch();
+
+console.log('current platform:', platform, arch);
 
 // 准备目录
 const buildTargetDir = '_opencv_build';
 if (!fs.existsSync(buildTargetDir)) exec('mkdir', [buildTargetDir]);
 
+/** @type {string} */
+let tarName;
+
 if (platform === 'darwin') {
-  const tarName = 'opencv-3.4.12-build-mac.xz';
-
-  if (!fs.existsSync(tarName)) {
-    exec('wget', [`https://unpkg.com/pilib-opencv-build@0.0.1/${tarName}`]);
-  }
-
-  exec('tar', ['xvJf', tarName, '-C', buildTargetDir]);
+  tarName = 'opencv-3.4.12-build-mac.xz';
+} else if (platform === 'linux' && arch === 'arm') {
+  tarName = 'opencv-3.4.12-build-linux-armv7l.xz';
+} else {
+  throw new Error(`not support ${platform} ${arch}`);
 }
+
+if (!fs.existsSync(tarName)) {
+  exec('wget', [`https://admin.shiluyizhan.com/gw/assets/${tarName}`]);
+}
+
+exec('tar', ['xvJf', tarName, '-C', buildTargetDir]);
 
 // 安装 opencv4nodejs
 exec('cnpm', ['i', 'opencv4nodejs@^5.6.0'], {
