cb3c2ff2e0e518472d23004f0e9def1f874f4c25
concefly <concefly@foxmail.com>
2020-12-15 14:37:47 +0800

    remove apt-wizard

--- packages/apt-wizard/src/type/fs.ts
+++
@@ -1,6 +0,0 @@
-export interface IFS {
-  readFileSync(path: string, options?: { encoding?: any }): any;
-  writeFileSync(path: string, data: any, options?: { encoding?: any }): void;
-  existsSync(path: string): boolean;
-  mkdirSync(path: string): void;
-}
